ed39a9bd8e32bdcb7c53656e192a46e9ebc7ac0d
Jan Skowronski <crianonim@gmail.com>
2019-08-11 11:24:44 +0100

    Move data to helper

--- src/App.js
+++ src/App.js
@@ -2,45 +2,7 @@ import React, { useState, useEffect } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import CodePanel from "./CodePanel.js";
-
-const start = [
-  {
-    shape: "circle",
-    attributes: { cx: 151, cy: 132, r: 52, fill: "red" }
-  },
-  {
-    shape: "circle",
-    attributes: {
-      cx: 101,
-      cy: 120,
-      r: 40,
-      fill: "pink"
-    }
-  },
-  {
-    shape: "circle",
-    attributes: {
-      cx: 601,
-      cy: 420,
-      r: 140,
-      fill: "green"
-    }
-  },
-  {
-    shape: "rect",
-    attributes: {
-      x: 40,
-      y: 320,
-      width: 140,
-      height: 200
-    }
-  }
-];
-const defaultValues = {
-  circle: '{"cx":50,"cy":30,"r":10}',
-  rect: '{"x":20,"y":50,"width":40,"height":20}',
-  polygon: '{"points":"10 10 45 15 20 35"}'
-};
+import {start,defaultValues} from "./lib/helper.js";
 
 const App = () => {
   const updateShapes = () => {
@@ -109,7 +71,20 @@ const App = () => {
         Add Shape
       </button>
       <CodePanel shapes={ shapes} svgAttrs={svgAttrs}  />
+      <div className="shape-row">
+        <span className="shape-type">svg</span>
+        <input
+              className="shape-attributes"
+              value={JSON.stringify(svgAttrs)}
+              onChange={e => {
+                try {
+                  const value=JSON.parse(e.target.value);
+                  setSvgAttrs(value);
+                } catch(err){
 
+                }
+              }} />
+      </div>
       <div>
         {shapesControlls.map((control, ind) => (
           <div className="shape-row" key={ind}>
