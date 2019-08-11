91b39546019d26ad3b60438d7a40f5ed9fc3f055
Jan Skowronski <crianonim@gmail.com>
2019-08-11 13:55:39 +0100

    Create new svg attr from dropdown

--- src/BasicAttrEditor.js
+++ src/BasicAttrEditor.js
@@ -1,12 +1,40 @@
 import React, { useState} from "react";
 
+const attrs={
+	"viewBox":{
+		def:"0 0 360 360"
+	},
+    	"fill":{
+		def:"pink"
+	},
+    	"stroke":{
+		def:"purple",
+	}
+}
+
 const BasicAttrEditor=(props)=>{
     const [controls,setControls]=useState(Object.entries(props.svgAttrs));
+    const unUsedAttrs=Object.keys(attrs).filter(key=>controls.every(control=>control[0]!==key));
+    const [addAttr,setAddAttr]=useState(unUsedAttrs[0])
+    console.log(unUsedAttrs); 
     return (
         <div className="flex-row">
-            {controls.map( ([key,value],i)=>{
-                return (
-                    <>
+	<select value={addAttr} onChange={(e)=>{
+	setAddAttr(e.target.value);
+	}}>
+	 { unUsedAttrs.map(key=>(<option>{key}</option>)) }
+	</select>
+
+	<button onClick={ ()=>{
+	    const newControls=[...controls,[addAttr,attrs[addAttr].def]]; 
+	    setControls(newControls)
+	    props.changed(Object.fromEntries(newControls));
+	}
+
+	}>New Attr</button>
+	{controls.map( ([key,value],i)=>{
+	    return (
+                    <span className="attr-pair">
                     <input onChange={(e)=>{
                         controls[i][0]=e.target.value;
                         setControls(controls.slice());
@@ -14,14 +42,14 @@ const BasicAttrEditor=(props)=>{
                     }}  value={key}></input>
                     <input onChange={(e)=>{
                         controls[i][1]=e.target.value;
-                        setControls(controls.slice());
+                       // setControls(controls.slice());
                         props.changed(Object.fromEntries(controls));
                     }} 
                     value={value}></input>
-                    </>
+                    </span>
                 )
             } )}
-
+		
         </div>
     )
 }
