0e2441e49e7f39b46d2e65d82ce9c3c71ae725c3
Jan Skowronski <crianonim@gmail.com>
2019-08-11 20:43:28 +0100

    Initial Margins

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import CodePanel from "./CodePanel.js";
 import BasicAttrEditor from "./BasicAttrEditor.js";
-import {start,defaultValues} from "./lib/helper.js";
+import { start, defaultValues } from "./lib/helper.js";
 
 const App = () => {
-  useEffect( ()=>{
+  useEffect(() => {
     console.log("RENDER");
     return () => {
       console.log("clean-up");
     };
   });
-  
+
   const [shapes, setShapes] = useState(start);
   const [newShape, setNewShape] = useState("circle");
-  const [svgAttrs,setSvgAttrs] = useState({viewBox:"0 0 720 720"});
+  const [svgAttrs, setSvgAttrs] = useState({ viewBox: "0 0 720 720" });
   return (
     <>
       <svg {...svgAttrs} className="Svg-view">
         {shapes.map((shape, i) => {
           const ShapeType = shape.shape;
-          return <ShapeType key={i} {...shape.attributes} />;
+          return <ShapeType onClick={(e)=>{
+            console.log(e.target);
+          }} key={i} {...shape.attributes} />;
         })}
       </svg>
       <select
@@ -39,61 +41,66 @@ const App = () => {
           console.log(newShape);
           setShapes([
             ...shapes,
-            { shape: newShape, attributes: defaultValues[newShape],id:Date.now() }
+            {
+              shape: newShape,
+              attributes: defaultValues[newShape],
+              id: Date.now()
+            }
           ]);
         }}
       >
         Add Shape
       </button>
-      <CodePanel shapes={ shapes} svgAttrs={svgAttrs}  />
-      <BasicAttrEditor element='svg' attrs={svgAttrs} changed={(attr)=>{
-        setSvgAttrs(attr);
-      }}/>
-        
+      <CodePanel shapes={shapes} svgAttrs={svgAttrs} />
+      <BasicAttrEditor
+        element="svg"
+        attrs={svgAttrs}
+        changed={attr => {
+          setSvgAttrs(attr);
+        }}
+      />
 
       <div className="shapes-list">
-      {shapes.map ( (shape,ind) => (
-	  <div key={shape.id} className="flex-row">
-	  <BasicAttrEditor element={shape.shape} attrs={shape.attributes} changed={(attrs)=>{
-	      shapes[ind].attributes=attrs;
-	      setShapes(shapes.slice());
-
-	  }} />
-	  <div>
-
-	  {ind > 0 ? (
-	      <button
-	      onClick={() => {
-		  let old = shapes[ind - 1];
-		  shapes[ind - 1] = shapes[ind];
-		  shapes[ind] = old;
-		  setShapes(shapes.slice());
-	      }}
-	      >
-	      up
-	      </button>
-	  ) : null}
-	  {ind < shapes.length - 1 ? (
-	      <button
-	      onClick={() => {
-		  let old = shapes[ind + 1];
-		  shapes[ind + 1] = shapes[ind];
-		  shapes[ind] = old;
-		  setShapes(shapes.slice());
-	      }}
-	      >
-	      down
-	      </button>
-	  ) : null}
-	  </div>
-
-
-	  </div>
-      ))}
-
-}
-              </div>
-
+        {shapes.map((shape, ind) => (
+          <div key={shape.id} className="flex-row">
+            <BasicAttrEditor
+              element={shape.shape}
+              attrs={shape.attributes}
+              changed={attrs => {
+                shapes[ind].attributes = attrs;
+                setShapes(shapes.slice());
+              }}
+            />
+            <div>
+              {ind > 0 ? (
+                <button
+                  onClick={() => {
+                    let old = shapes[ind - 1];
+                    shapes[ind - 1] = shapes[ind];
+                    shapes[ind] = old;
+                    setShapes(shapes.slice());
+                  }}
+                >
+                  up
+                </button>
+              ) : null}
+              {ind < shapes.length - 1 ? (
+                <button
+                  onClick={() => {
+                    let old = shapes[ind + 1];
+                    shapes[ind + 1] = shapes[ind];
+                    shapes[ind] = old;
+                    setShapes(shapes.slice());
+                  }}
+                >
+                  down
+                </button>
+              ) : null}
+            </div>
+          </div>
+        ))}
+        }
+      </div>
     </>
   );
 };
--- src/BasicAttrEditor.js
+++ src/BasicAttrEditor.js
@@ -1,61 +1,69 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
-const attrsData={
-	"viewBox":{
-		def:"0 0 360 360"
-	},
-    	"fill":{
-		def:"pink"
-	},
-    	"stroke":{
-		def:"purple",
-	}
-}
+const attrsData = {
+  viewBox: {
+    def: "0 0 360 360"
+  },
+  fill: {
+    def: "pink"
+  },
+  stroke: {
+    def: "purple"
+  }
+};
 
-const BasicAttrEditor=(props)=>{
-    const [controls,setControls]=useState(Object.entries(props.attrs));
-    const unUsedAttrs=Object.keys(attrsData).filter(key=>controls.every(control=>control[0]!==key));
-    const [addAttr,setAddAttr]=useState(unUsedAttrs[0])
-    console.log(props.element,unUsedAttrs); 
-    return (
-        <div className="flex-row">
-	<span>{props.element}</span>
-	<select value={addAttr} onChange={(e)=>{
-	    console.log(e.target.value);
-	setAddAttr(e.target.value);
-	}}>
-	 { unUsedAttrs.map(key=>(<option key={key} >{key}</option>)) }
-	</select>
+const BasicAttrEditor = props => {
+  const [controls, setControls] = useState(Object.entries(props.attrs));
+  const unUsedAttrs = Object.keys(attrsData).filter(key =>
+    controls.every(control => control[0] !== key)
+  );
+  const [addAttr, setAddAttr] = useState(unUsedAttrs[0]);
+  console.log(props.element, unUsedAttrs);
+  return (
+    <div className="flex-row flex-wrap attr-wrapper">
+      <span>{props.element}</span>
+      <select
+        value={addAttr}
+        onChange={e => {
+          console.log(e.target.value);
+          setAddAttr(e.target.value);
+        }}
+      >
+        {unUsedAttrs.map(key => (
+          <option key={key}>{key}</option>
+        ))}
+      </select>
 
-	<button onClick={ ()=>{
-	  console.log({addAttr})
-	    const newControls=[...controls,[addAttr,attrsData[addAttr].def]]; 
-	    setControls(newControls);
-	    setAddAttr(unUsedAttrs.filter(key=>key!==addAttr)[0])
-	    props.changed(Object.fromEntries(newControls));
-	}
-
-	}>New Attr</button>
-	{controls.map( ([key,value],i)=>{
-	    return (
-                    <span key={key} className="attr-pair">
-                    <input onChange={(e)=>{
-                        controls[i][0]=e.target.value;
-                        setControls(controls.slice());
-                        props.changed(Object.fromEntries(controls));
-                    }} style={{width:key.length/2+"rem"}} value={key}></input>
-                    <input onChange={(e)=>{
-                        controls[i][1]=e.target.value;
-                        setControls(controls.slice());
-                        props.changed(Object.fromEntries(controls));
-                    }} 
-                    value={value}></input>
-                    </span>
-                )
-            } )}
-		
-        </div>
-    )
-}
+      <button
+        onClick={() => {
+          console.log({ addAttr });
+          const newControls = [...controls, [addAttr, attrsData[addAttr].def]];
+          setControls(newControls);
+          setAddAttr(unUsedAttrs.filter(key => key !== addAttr)[0]);
+          props.changed(Object.fromEntries(newControls));
+        }}
+      >
+        +
+      </button>
+      {controls.map(([key, value], i) => {
+        return (
+          <span key={key} className="attr-pair">
+            
+            <span className="key-name">{key}</span>
+            <input
+              onChange={e => {
+                controls[i][1] = e.target.value;
+                setControls(controls.slice());
+                props.changed(Object.fromEntries(controls));
+            }}
+            style={{ width: (value+"").length / 2 + "rem" }}
+              value={value}
+            />
+          </span>
+        );
+      })}
+    </div>
+  );
+};
 
 export default BasicAttrEditor;
