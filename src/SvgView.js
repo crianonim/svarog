1f2398f28ac5f5d105afb1fdaf2f2950e712a461
Jan Skowronski <crianonim@gmail.com>
2019-08-13 07:13:43 +0100

    Add zoom radios to SvgView Fixes #3

--- src/SvgView.js
+++ src/SvgView.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './SvgView.css';
 
+
+
+
 const SvgView = (props) => {
+    const [zoom,setZoom] = useState(400);
+
+    const setZoomHandler = zoom => () => setZoom(zoom);
     return (
-        <>
-        <svg {...props.attrs} className="Svg-view">
+        <div className="svg-display">
+            <div className="svg-zoom-control flex-row">
+                Zoom:
+                <label>
+                  <input type="radio" checked={zoom===100} onChange={setZoomHandler(100)} name="zoom-radios" />
+                  100
+                </label>
+                <label>
+                  <input type="radio" checked={zoom===200} onChange={setZoomHandler(200)} name="zoom-radios" />
+                    200
+                </label>
+                <label>
+                  <input type="radio" checked={zoom===400} onChange={setZoomHandler(400)} name="zoom-radios" />
+                    400
+                </label>
+                <label>
+                  <input type="radio" checked={zoom===800} onChange={setZoomHandler(800)} name="zoom-radios" />
+                    800
+                </label>
+            </div>
+        <svg {...props.attrs} className="Svg-view" style={{width:zoom}}>
         {props.shapes.map((shape, i) => {
           const ShapeType = shape.shape;
           return <ShapeType data-id={shape.id} onClick={(e)=>{
@@ -12,7 +37,7 @@ const SvgView = (props) => {
           }} key={i} {...shape.attributes} />;
         })}
       </svg>
-        </>
+        </div>
     )
 }
 
