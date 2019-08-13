6ebd881fb4ef09d024b72463ca7a2bc35d223e61
Jan Skowronski <crianonim@gmail.com>
2019-08-13 06:49:44 +0100

    SvgView setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-// import logo from './logo.svg';
+import { start, defaultValues } from "./lib/helper.js";
 import "./App.css";
+
 import CodePanel from "./CodePanel.js";
 import BasicAttrEditor from "./BasicAttrEditor.js";
-import { start, defaultValues } from "./lib/helper.js";
 import ShapeItem from "./ShapeItem";
 import InputTextArea from './InputTextArea';
+import SvgView from './SvgView.js';
 
 const App = () => {
   useEffect(() => {
@@ -32,6 +33,7 @@ const App = () => {
 
   return (
     <>
+      <SvgView shapes={shapes} attrs={svgAttrs} />
       <svg {...svgAttrs} className="Svg-view">
         {shapes.map((shape, i) => {
           const ShapeType = shape.shape;
---
+++ src/SvgView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SvgView.css';
+
+const SvgView = (props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default SvgView;
