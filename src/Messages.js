df85e332b07e85a8872868ae76637b6e4844ebe1
Jan Skowronski <crianonim@gmail.com>
2019-08-16 08:20:20 +0100

    Parse message to determine type relates #11

--- src/Messages.js
+++ src/Messages.js
@@ -2,11 +2,20 @@ import React, {useState} from 'react';
 import './Messages.css';
 
 const Messages = (props)=>{
+    const [type,msg] = parseMessage(props.message);
     return (
     <div className="bordered margined" onClick={props.dismiss}>
-     {props.message}    
+     <p className={"message message-type-"+type}>{msg}</p>    
     </div>
     );
 }
-
+const parseMessage = message => {
+    let type="i";
+    let msg=message;
+    if (message[1]==="#"){
+        type=message[0].toLowerCase();
+        msg=message.substring(2);
+    }
+    return [type,msg]
+}
 export default Messages
