86e3105a95c7a2957869f2971ba0c2a4f13d7edf
Jan Skowronski <crianonim@gmail.com>
2019-09-17 09:39:50 +0100

    Change inputs to Bulma ones

--- src/AddShape.js
+++ src/AddShape.js
@@ -5,22 +5,25 @@ import {defaultValues} from './lib/helper.js'
 const AddShape = (props) => {
     const [newShape, setNewShape] = useState("circle");
     return (
-        <>
+        <span className="field has-addons">
+         <span className="control select is-small">
+
          <select
           value={newShape}
           onChange={e => {
-           setNewShape(e.target.value);
+            setNewShape(e.target.value);
           }}
-         >{
-          Object.keys(defaultValues).map(shapeName=>(<option key={shapeName}>{shapeName}</option>))
-         }
+          >{
+            Object.keys(defaultValues).map(shapeName=>(<option key={shapeName}>{shapeName}</option>))
+          }
          </select>
-      <button className="button is-small"
+          </span>
+      <button className="button control is-small is-primary"
         onClick={() => {
           props.addShape(newShape)
         }}
       >Add Shape</button>
-        </>
+        </span>
     );
 }
 
--- src/ShapeItem.js
+++ src/ShapeItem.js
@@ -8,16 +8,16 @@ const ShapeItem = ({shape,duplicate,shapeUp,shapeDown,setSelectedShape,changed})
         <div className="move-up-down-buttons">
           <button onClick={()=>{
             duplicate(shape);
-          }} className="button is-small"
+          }} className="button is-small is-primary"
           title="Duplicate Shape"
           >
             *
           </button>
-            <button title="Move Up"  className="move-up-button"
+            <button title="Move Up"  className="move-up-button button is-small is-info"
               onClick={() => {
                   shapeUp(shape);
               }}>&#x21E7;</button>
-            <button title="Move Down" className="move-down-button" onClick={() => {
+            <button title="Move Down" className="move-down-button  button is-small is-info" onClick={() => {
                 shapeDown(shape);
               }}
             >&#x21E9;</button>
