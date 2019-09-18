b8b712990effeb24e85bd36eed98979f7e50aa64
Jan Skowronski <crianonim@gmail.com>
2019-09-18 08:26:45 +0100

    Make zoom tabs is-active reflect current zoom

--- src/SvgView.js
+++ src/SvgView.js
@@ -13,9 +13,9 @@ const SvgView = (props) => {
           <div className="panel-heading">Your SVG</div>
           <p className="panel-tabs">
             <span className="align-self-center">Zoom:</span>
-            <a onClick={setZoomHandler(100)}>100p</a>
-            <a onClick={setZoomHandler(200)}>200px</a>
-            <a onClick={setZoomHandler(400)} className="is-active">400px</a>
+            <a onClick={setZoomHandler(100)} className={zoom===100?"is-active":""}>100p</a>
+            <a onClick={setZoomHandler(200)} className={zoom===200?"is-active":""}>200px</a>
+            <a onClick={setZoomHandler(400)} className={zoom===400?"is-active":""}>400px</a>
             {/* <a onClick={setZoomHandler(800)}>800px</a> */}
           </p>
          <div className="panel-block">
