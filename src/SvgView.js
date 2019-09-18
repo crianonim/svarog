1ae6cb2dbc9493552d1298745f27cce8ddd7edfe
Jan Skowronski <crianonim@gmail.com>
2019-09-18 08:17:23 +0100

    Fit SvgView in a Panel

--- src/SvgView.js
+++ src/SvgView.js
@@ -9,36 +9,25 @@ const SvgView = (props) => {
 
     const setZoomHandler = zoom => () => setZoom(zoom);
     return (
-        <div className="svg-display margined">
-            <div className="svg-zoom-control flex-row flex-align-center">
-               
-                <span >Zoom:</span>
-
-                <label>
-                  <input type="radio" checked={zoom===100} onChange={setZoomHandler(100)} name="zoom-radios" />
-                  100
-                </label>
-                <label>
-                  <input type="radio" checked={zoom===200} onChange={setZoomHandler(200)} name="zoom-radios" />
-                    200
-                </label>
-                <label>
-                  <input type="radio" checked={zoom===400} onChange={setZoomHandler(400)} name="zoom-radios" />
-                    400
-                </label>
-                <label>
-                  <input type="radio" checked={zoom===800} onChange={setZoomHandler(800)} name="zoom-radios" />
-                    800
-                </label>
-            </div>
-        <svg {...props.attrs} className="Svg-view" onClick={console.log} style={{width:zoom}}>
+        <div className="panel">
+          <div className="panel-heading">Your SVG</div>
+          <p className="panel-tabs">
+            <span className="align-self-center">Zoom:</span>
+            <a onClick={setZoomHandler(100)}>100p</a>
+            <a onClick={setZoomHandler(200)}>200px</a>
+            <a onClick={setZoomHandler(400)} className="is-active">400px</a>
+            {/* <a onClick={setZoomHandler(800)}>800px</a> */}
+          </p>
+         <div className="panel-block">
+          <svg {...props.attrs} className="Svg-view" onClick={console.log} style={{width:zoom}}>
         {props.shapes.map((shape, i) => {
           const ShapeType = shape.shape;
           return <ShapeType data-id={shape.id} onClick={(e)=>{
             props.setSelectedShape(shape.id)
           }} key={i} {...shape.attributes} />;
         })}
-      </svg>
+          </svg>
+         </div>
         </div>
     )
 }
