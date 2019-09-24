8c50d428995dd99bab8ad505723e955ba0eaf8fa
Jan Skowronski <crianonim@gmail.com>
2019-09-24 18:28:59 +0100

    Add AttrList tests

---
+++ src/_tests_/AttrList.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import AttrList from "../AttrList";
+
+it("matches a snapshot",()=>{
+    const wrapper=shallow(<AttrList element="circle" attrs={{"x":10,"y":20,"r":30}}/>) 
+    expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it("has a shape name",()=>{
+    const wrapper=shallow(<AttrList element="circle" attrs={{"x":10,"y":20,"r":30}}/>) 
+    expect(wrapper.find('div > span:first-child').text()).toBe("circle")
+})
+
+it("has 3 attributes",()=>{
+    const wrapper=shallow(<AttrList element="circle" attrs={{"x":10,"y":20,"r":30}}/>) 
+    const attrs=wrapper.find('div > span').slice(1)
+    expect(attrs.length).toBe(3)
+})
