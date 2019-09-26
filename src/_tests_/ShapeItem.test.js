ce0efb739778fb5922f0fe1c549c535e1164ba52
Jan Skowronski <crianonim@gmail.com>
2019-09-26 09:42:53 +0100

    Add ShapeItem tests

---
+++ src/_tests_/ShapeItem.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import ShapeItem from "../components/ShapeItem";
+
+it("matches a snapshot",()=>{
+    const wrapper=shallow(<ShapeItem shape={{shape:"circle",attributes:{"x":10,"y":20,"r":30}}}/>) 
+    expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+it("duplicate button can be clicked",()=>{
+    const duplicate=jest.fn();
+    const wrapper=shallow(<ShapeItem duplicate={duplicate} shape={{shape:"circle",attributes:{"x":10,"y":20,"r":30}}}/>) 
+    wrapper.find(".move-up-down-buttons button").first().simulate('click');
+    expect(duplicate).toHaveBeenCalledTimes(1);
+})
