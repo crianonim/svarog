61a3b7dfdc2f42dbc0275dfaeaeb58d941378ce1
Jan Skowronski <crianonim@gmail.com>
2019-09-26 09:21:18 +0100

    Update files locations

--- src/_tests_/CodePanel.test.js
+++ src/_tests_/CodePanel.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
-import CodePanel from "../../components/CodePanel";
+import CodePanel from "../components/CodePanel";
 
 it("matches a snapshrot",()=>{
     const wrapper=shallow(<CodePanel svgAttrs={{fill:"black"}} shapes={[{shape:"circle",attributes:{"x":10,"y":20,"r":30}}] } />) 
