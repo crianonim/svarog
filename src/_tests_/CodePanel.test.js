e85885249a320bdbe846f4e0b7cc98810b7fb12d
Jan Skowronski <crianonim@gmail.com>
2019-09-27 09:18:17 +0100

    Add test to CodePanel

--- src/_tests_/CodePanel.test.js
+++ src/_tests_/CodePanel.test.js
@@ -7,3 +7,12 @@ it("matches a snapshrot",()=>{
     const wrapper=shallow(<CodePanel svgAttrs={{fill:"black"}} shapes={[{shape:"circle",attributes:{"x":10,"y":20,"r":30}}] } />) 
     expect(toJson(wrapper)).toMatchSnapshot();
 });
+it("produces correct rendition of svg code",()=>{
+    const wrapper=shallow(<CodePanel svgAttrs={{fill:"black"}} shapes={[{shape:"circle",attributes:{"x":10,"y":20,"r":30}}] } />) 
+    expect(wrapper.find('pre').text()).toBe(`
+<svg fill="black"> 
+ <circle x="10" y="20" r="30"/>
+</svg>
+  `)
+})
+
