ffaf17abce8900df1304e4f777c119d204ca4f1f
Jan Skowronski <crianonim@gmail.com>
2019-09-27 09:56:30 +0100

    add test for delete button

--- src/_tests_/ShapeList.test.js
+++ src/_tests_/ShapeList.test.js
@@ -18,3 +18,25 @@ it("matches a snapshrot", () => {
   );
   expect(toJson(wrapper)).toMatchSnapshot();
 });
+
+it("deletes an item from list on delete button click",()=>{
+    const moveShape = jest.fn();
+    const setShapes = jest.fn();
+    const setSelectedShape = jest.fn();
+    const wrapper = shallow(
+      <ShapesList
+        selectedShape={1}
+        setSelectedShape={setSelectedShape}
+        moveShape={moveShape}
+        setShapes={setShapes}
+        svgAttrs={{ fill: "black" }}
+        shapes={[{ id:1,shape: "circle", attributes: { x: 10, y: 20, r: 30 } }]}
+      />
+    );
+    wrapper.find('button').simulate('click');
+    expect(setShapes).toBeCalledTimes(1);
+    expect(setShapes).toBeCalledWith([]);
+    expect(setSelectedShape).toBeCalledTimes(1);
+    expect(setSelectedShape).toBeCalledWith(null);
+
+})
